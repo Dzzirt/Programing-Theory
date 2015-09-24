c939441ae7028dd06088856069e8297a14ad2402
Nikita Kuzin <nikitakyza@gmail.com>
2015-09-25 00:07:11 +0300

    Broken Direction

--- Snake/Snake/main.cpp
+++ Snake/Snake/main.cpp
@@ -7,15 +7,16 @@ using namespace sf;
 struct SnakeParts
 {
 	float xPos, yPos, dx = 0, dy = 0;
-	float dxInt = 0, dyInt = 0;
+	int dir = 0;
+	bool draw;
 	String fileName;
 	Image image;
 	Texture texture;
 	Sprite sprite;
-	int dir = 0;
-	bool draw;
-	float speed = 0.1;
-
+	int predX = 0;
+	int predY = 0;
+	int predDir;
+	
 	SnakeParts()
 	{
 		image.loadFromFile("images/snakePart.png");
@@ -26,18 +27,27 @@ struct SnakeParts
 
 	void update()
 	{
+		
 		switch (dir)
 		{
 		case 0:	sprite.move(19, 0);
+			predX = xPos;
+			predY = yPos;
 			xPos += 19;
 			break;
 		case 1: sprite.move(-19, 0);
+			predX = xPos;
+			predY = yPos;
 			xPos -= 19;
 			break;
 		case 2: sprite.move(0, 19);
+			predY = yPos;
+			predX = xPos;
 			yPos += 19;
 			break;
 		case 3: sprite.move(0, -19);
+			predY = yPos;
+			predX = xPos;
 			yPos -= 19;
 			break;
 		}
@@ -64,26 +74,40 @@ struct Apple
 	}
 };
 
+struct Map
+{
+	Image image;
+	Texture texture;
+	Sprite sprite;
+
+	Map()
+	{
+	    image.loadFromFile("images/map.png");
+		texture.loadFromImage(image);
+		sprite.setTexture(texture);
+	}
+};
+void MapDraw()
+{
+	
+
+	
+}
 int main()
 {
-	Clock clock;
 	RenderWindow window(VideoMode(380, 380), "SFML works!");
-	struct SnakeParts parts[50];
-	int potentialX = 0;
-	int potentialY = 0;
+	Clock clock;
+	Map map;
+	Apple apple;
+	SnakeParts parts[50];
+	int potentialX, potentialY, potentialDir, partCount = 0;
 	float timeCounter = 0;
-	int partCount = 0;
 	parts[0].xPos = 0;
 	parts[0].yPos = 0;
 	parts[0].sprite.setPosition(0, 0);
 	parts[0].draw = true;
-	Apple apple;
-	Image map_image;
-	map_image.loadFromFile("images/map.png");
-	Texture map;
-	map.loadFromImage(map_image);
-	Sprite s_map;
-	s_map.setTexture(map);
+	
+	
 
 	
 	while (window.isOpen())
@@ -100,28 +124,58 @@ int main()
 			if (event.type == Event::Closed)
 				window.close();
 		}
+
 		if (Keyboard::isKeyPressed(Keyboard::W))
 		{
+			parts[0].predDir = parts[0].dir;
 			parts[0].dir = 3;
 		}
+
 		if (Keyboard::isKeyPressed(Keyboard::S))
 		{
+			parts[0].predDir = parts[0].dir;
 			parts[0].dir = 2;
 		}
+
 		if (Keyboard::isKeyPressed(Keyboard::A))
 		{
+			parts[0].predDir = parts[0].dir;
 			parts[0].dir = 1;
 		}
+
 		if (Keyboard::isKeyPressed(Keyboard::D))
 		{
+			parts[0].predDir = parts[0].dir;
 			parts[0].dir = 0;
 		}
-		if (timeCounter > 0.1)
+
+		if (timeCounter > 0.2)
 		{
-			potentialX = parts[partCount].xPos;
-			potentialY = parts[partCount].yPos;
-			parts[partCount].update();
-			timeCounter -= 0.1;
+			potentialX = parts[partCount].xPos; // потенц Х след части змейки
+			potentialY = parts[partCount].yPos; // потенц У след части змейки
+			potentialDir = parts[partCount].dir; // потенц Направление 
+			timeCounter -= 0.2;
+			
+			for (int partCount = 1; partCount < 50; partCount++)
+			{
+				parts[partCount].xPos = parts[partCount - 1].predX;
+				parts[partCount].yPos = parts[partCount - 1].predY;
+				//parts[partCount].dir = parts[partCount - 1].predDir;
+				parts[partCount].sprite.setPosition(parts[partCount].xPos, parts[partCount].yPos);
+				//parts[partCount].dir = parts[partCount - 1].dir;
+			}
+			parts[0].update();
+			for (int i = 1; i < 50; i++)
+			{
+				if (i > 1 && parts[i].dir != parts[i].predDir)
+				{
+					parts[i].dir = parts[i].predDir;
+				}
+				//else parts[i].dir = parts[i - 1].dir;
+				parts[i].update();
+				
+			}
+			
 			if(apple.xPos == parts[0].xPos && apple.yPos == parts[0].yPos)
 			{
 				partCount++;
@@ -131,19 +185,28 @@ int main()
 				parts[partCount].xPos = potentialX;
 				parts[partCount].yPos = potentialY;
 				parts[partCount].sprite.setPosition(parts[partCount].xPos, parts[partCount].yPos);
+				parts[partCount].dir = potentialDir;
 				parts[partCount].draw = 1;
 				
 			}
+			
+			
 		}
 
 		window.clear();
+
+		//MAP draw
+
 		for (int i = 0; i < MAP_HEIGHT; i++)
 			for (int j = 0; j < MAP_WIDTH; j++)
 			{
-				if (TileMap[i][j] == ' ') s_map.setTextureRect(IntRect(0, 0, 19, 19));
-				s_map.setPosition(j * 19, i * 19);
-				window.draw(s_map);
+				if (TileMap[i][j] == ' ') map.sprite.setTextureRect(IntRect(0, 0, 19, 19));
+				map.sprite.setPosition(j * 19, i * 19);
+				window.draw(map.sprite);
 			}
+
+		//Snake draw
+		
 		for (int i = 0; i < 50; ++i)
 		{
 			if (parts[i].draw == true)
@@ -151,8 +214,11 @@ int main()
 				window.draw(parts[i].sprite);
 			}
 		}
-		window.draw(apple.sprite);
 
+		//Apple draw
+
+		window.draw(apple.sprite);
+		
 		window.display();
 	}
 
